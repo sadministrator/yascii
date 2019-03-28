5ff9e7813c7e498bebbb071a25fce60831b14a08
marco <marco@marcoserrano.com>
2019-03-28 02:09:29 -0600

    Copied an <img> to a <canvas> and used canvas functions to extract pixel data.
    Pixel data is then parsed and used to change the fillStyle of text drawn onto
    a second canvas.

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-var canvas = document.getElementById('ascii');
+$(document).ready(function() {
+  var canvas = document.getElementById('orig');
+  var canvas2 = document.getElementById('ascii');
 
-if(canvas.getContext) {
-var ctx = canvas.getContext('2d');
+  if(canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+    var ctx2 = canvas2.getContext('2d');
 
-ctx.fillStyle = 'rgb(200, 0, 0)';
-ctx.fillRect(10, 10, 50, 50);
+    var img = document.getElementById('pic');
 
-ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-ctx.fillRect(30, 30, 50, 50);
-} else { // canvas not supported
+    canvas.width = img.width;
+    canvas.height = img.height;
 
-}
+    canvas2.width = 1300;
+    canvas2.height = 1000;
+
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+    var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
+    ctx2.font = '60px monospace';
+
+    for (i = 0; i < img.height; i++){
+      for (j = 0; j < img.width; j++){
+        ctx2.fillStyle = 'rgba(' + getPixelXY(imgData, j, i) + ')';
+        //ctx2.strokeStyle =
+        ctx2.fillText('0', j*3, i*3);
+      }
+    }
+
+    //canvas2.width = ctx2.measureText();
+
+    console.log('width: ' + img.width + "     height: " + img.height);
+  } else { // canvas not supported
+    document.getElementById("text").innerHTML = "Error, bitch.";
+  }
+
+  function getPixel(imgData, index) {
+    var i = index*4, d = imgData.data;
+    return [d[i], d[i+1], d[i+2], d[i+3]]; // [r, g, b, a]
+  }
+
+  function getPixelXY(imgData, x, y) {
+    return getPixel(imgData, y * imgData.width + x)
+  }
+});
